b6d08ad8a746072479e63a6f66cf56b6ae7842ec
Eluzaivito <92304736+Eluzaivito@users.noreply.github.com>
2022-05-14 22:35:21 +0700

    validation

--- app/Models/camp.php
+++ app/Models/camp.php
@@ -5,10 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\softDeletes;
+use App\Models\Checkout;
+use Auth;
 
-class camp extends Model
+class Camp extends Model
 {
     use HasFactory, softDeletes;
 
     protected $fillable = ['title', 'price'];
+
+    public function getIsRegisteredAttribute()
+    {
+        if (!Auth::check()) {
+            return false;
+        }
+
+        return checkout::whereCampId($this->id)->whereUserId(Auth::id())->exists();
+    }
 }
